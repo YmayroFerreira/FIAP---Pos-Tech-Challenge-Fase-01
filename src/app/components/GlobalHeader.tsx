31e9a567f992faac4dc7f4c8f55596dcc417bb60
Ymayro <ymayro.ferreira@gmail.com>
2025-07-05 13:54:14 -0300

    global.css tailwind configuration

---
+++ src/app/components/GlobalHeader.tsx
@@ -0,0 +1,8 @@
+export default function GlobalHeader () {
+
+    return (
+        <div className="bg-gradient-bb text-bb-green p-6 border-4 border-indigo-500k">
+            My Username
+        </div>
+    )
+}
