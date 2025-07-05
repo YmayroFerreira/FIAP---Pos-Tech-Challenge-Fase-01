4e6f6ca8bf6cb4b588ea447e1bfcff65a72ae400
Ymayro <ymayro.ferreira@gmail.com>
2025-07-05 15:15:47 -0300

    added user icon and adjusted header component

--- src/app/components/GlobalHeader.tsx
+++ src/app/components/GlobalHeader.tsx
@@ -1,8 +1,18 @@
+import Image from "next/image";
+
 export default function GlobalHeader () {
 
     return (
-        <div className="bg-gradient-bb text-bb-green p-6 border-4 border-indigo-500k">
-            My Username
+        <div className="bg-gradient-bb text-white p-4 border-4 border-indigo-500k flex justify-end items-center gap-3">
+            <span>My Username</span>
+            <Image
+            className="pr-3"
+                src="/user-icon.svg"
+                alt="Next.js logo"
+                width={40}
+                height={40}
+                priority
+            />
         </div>
     )
 }
