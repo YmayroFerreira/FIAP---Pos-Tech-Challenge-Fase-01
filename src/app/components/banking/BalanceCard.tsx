9fda0154a40f4c32e9a77ae3e676b69bc8739232
Ymayro <ymayro.ferreira@gmail.com>
2025-07-07 15:13:07 -0300

    added hero icons and adjusted component

--- src/app/components/banking/BalanceCard.tsx
+++ src/app/components/banking/BalanceCard.tsx
@@ -1,14 +1,37 @@
+"use client";
+
+import { useState } from "react";
+import { EyeIcon, EyeSlashIcon } from "@heroicons/react/24/solid";
+
 export default function BalanceCard() {
+  const [showBalance, setShowBalance] = useState(false);
+  const currentBalance = "2.500,00";
+  const toogleVisibility = () => {
+    setShowBalance(!showBalance);
+  };
   return (
-    <div className="bg-gradient-bb text-white p-4 w-full md:w-3/4 lg:w-1/2 min-h-100">
+    <div className="bg-gradient-bb text-white p-4 w-full md:w-3/4 lg:w-1/2 min-h-100 text-2xl rounded-lg">
       <div className="flex flex-col items-start">
-        <p>Olá, Username :)</p>
-        <p>Quinta-feira, 08/07/2025</p>
+        <p className="font-bold mb-4">Olá, Username ! :)</p>
+        <p className="text-base">Quinta-feira, 08/07/2025</p>
       </div>
       <div className="flex flex-col items-end">
-        <p>Saldo ICONE</p>
+        <p>
+          Saldo
+          {showBalance ? (
+            <EyeIcon
+              className="size-6 text-bb-red"
+              onClick={toogleVisibility}
+            />
+          ) : (
+            <EyeSlashIcon
+              className="size-6 text-bb-red"
+              onClick={toogleVisibility}
+            />
+          )}
+        </p>
         <p>Conta Corrente</p>
-        <p>R$ 2.500,00</p>
+        {showBalance ? <p>**********</p> : <p>{currentBalance}</p>}
       </div>
     </div>
   );
