ad3203795dea2256cd2f4d2a0e5192df6af4c825
Walteann <walteann3@gmail.com>
2025-09-30 21:13:03 -0300

    fix: removido hardcode apiurl

--- src/services/api-config.ts
+++ src/services/api-config.ts
@@ -1,5 +1,4 @@
-// export const API_BASE_URL = process.env.NEXT_PUBLIC_API_URL;
-export const API_BASE_URL = 'https://backend-392021924812.southamerica-east1.run.app';
+export const API_BASE_URL = process.env.NEXT_PUBLIC_API_URL;
 
 // armazena o token
 export let authToken: string | null = null;
