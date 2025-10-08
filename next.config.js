6cd865510d3cb56c0da7d32a4b05945b4aed68f9
Ymayro <ymayro.ferreira@gmail.com>
2025-10-07 22:31:55 -0300

    ajuste de next.config

--- next.config.js
+++ next.config.js
@@ -2,6 +2,8 @@ const STORE_HOST = process.env.STORE_HOST || "localhost";
 const STORE_PORT = process.env.STORE_PORT || 3002;
 
 const STORE_URL = `http://${STORE_HOST}:${STORE_PORT}`;
+HOMEPAGE_URL = `https://bytebank-homepage.vercel.app`
+
 // const HOMEPAGE_URL = 'http://localhost:3003'
 // const HOMEPAGE_URL = `http://${HOMEPAGE_HOST}:${HOMEPAGE_PORT}`;
 
@@ -17,11 +19,13 @@ const nextConfig = {
 
       {
         source: "/homepage",
-        destination: `https://bytebank-homepage.vercel.app/homepage`,
+        // destination: `https://bytebank-homepage.vercel.app/homepage`,
+        destination: `${HOMEPAGE_URL}/homepage`,
       },
       {
         source: "/homepage/:path*",
-        destination: `https://bytebank-homepage.vercel.app/homepage/:path*`,
+        // destination: `https://bytebank-homepage.vercel.app/homepage/:path*`,
+        destination: `${HOMEPAGE_URL}/homepage/:path*`,
       },
     
     ];
