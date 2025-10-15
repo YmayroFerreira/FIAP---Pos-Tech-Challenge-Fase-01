5860ae06b2ff2c778d9fc00d1cc4f3e8c1de9715
Walteann <walteann3@gmail.com>
2025-10-15 20:19:31 -0300

    chore: adjusment environments variables

--- next.config.js
+++ next.config.js
@@ -1,8 +1,4 @@
-const STORE_HOST = process.env.STORE_HOST || "localhost";
-const STORE_PORT = process.env.STORE_PORT || 3002;
 
-const STORE_URL = `http://${STORE_HOST}:${STORE_PORT}`;
-const HOMEPAGE_URL = `https://bytebank-homepage.vercel.app`;
 
 /** @type {import('next').NextConfig} */
 const nextConfig = {
@@ -10,19 +6,12 @@ const nextConfig = {
   async rewrites() {
     return [
       {
-        source: "/store",
-        destination: `${STORE_URL}/store`,
-      },
-
-      {
         source: "/homepage",
-        // destination: `https://bytebank-homepage.vercel.app/homepage`,
-        destination: `${HOMEPAGE_URL}/homepage`,
+        destination: `${process.env.NEXT_PUBLIC_HOMEPAGE_URL}/homepage`,
       },
       {
         source: "/homepage/:path*",
-        // destination: `https://bytebank-homepage.vercel.app/homepage/:path*`,
-        destination: `${HOMEPAGE_URL}/homepage/:path*`,
+        destination: `${process.env.NEXT_PUBLIC_HOMEPAGE_URL}/homepage/:path*`,
       },
     ];
   },
